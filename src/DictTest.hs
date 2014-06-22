2c90bf9061e41fd52f2ab5a26a0a0970d9ea3e99
Conal Elliott <conal@conal.net>
2014-06-22 11:07:16 -0700

    tidying

--- src/DictTest.hs
+++ src/DictTest.hs
@@ -1,15 +1,9 @@
--- {-# LANGUAGE #-}
 {-# OPTIONS_GHC -Wall #-}
-
-{-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
--- {-# OPTIONS_GHC -fno-warn-unused-binds   #-} -- TEMP
-
 ----------------------------------------------------------------------
 -- |
 -- Module      :  DictTest
--- Copyright   :  (c) 2014 Tabula, Inc.
 -- 
--- Maintainer  :  conal@tabula.com
+-- Maintainer  :  conal@conal.net
 -- Stability   :  experimental
 -- 
 -- Test HERMIT-based dictionary construction
@@ -19,11 +13,10 @@ module DictTest where
 
 import Control.Category ((>>>))
 
-import HERMIT.Dictionary hiding (externals) -- re-exports HERMIT.Dictionary.*
-import HERMIT.External (External,ExternalName,external,(.+),CmdTag(Loop))
+import HERMIT.Dictionary hiding (externals)
+import HERMIT.External (External,external)
 import HERMIT.GHC
 import HERMIT.Kure
-import HERMIT.Monad (saveDef,newIdH,Label)
 import HERMIT.Plugin (hermitPlugin,phase,interactive)
 
 -- | e ==> succ e, if e has a type in Enum
