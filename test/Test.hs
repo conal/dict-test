5232a1d842322727b534df5741055638ecc52908
Conal Elliott <conal@conal.net>
2014-06-23 10:26:52 -0700

    tweak Test.hs

--- test/Test.hs
+++ test/Test.hs
@@ -8,8 +8,18 @@
 
 module Test where
 
+-- Odd: Without a mention of succ somewhere in this module, buildDictionary
+-- fails silently, producing no bindings.
+foo :: ()
+foo = succ ()
+
 test1 :: Bool
 test1 = False
 
 test2 :: Int
 test2 = 3
+
+-- The 'succ' transformation should fail for this one. Instead, buildDictionary
+-- fails silently, producing no bindings.
+test3 :: String
+test3 = "oops!"
