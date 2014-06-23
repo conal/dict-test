8a394f4bf7cab481dcbe3368043dd2918b8c0439
Conal Elliott <conal@conal.net>
2014-06-23 11:09:19 -0700

    tweaked buildDictionaryT when MyBuildDict

--- src/DictTest.hs
+++ src/DictTest.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -Wall #-}
 ----------------------------------------------------------------------
 -- |
@@ -9,15 +10,28 @@
 -- Test HERMIT-based dictionary construction
 ----------------------------------------------------------------------
 
+-- #define MyBuildDict
+
 module DictTest where
 
 import Control.Category ((>>>))
 
-import HERMIT.Dictionary hiding (externals)
+import HERMIT.Dictionary hiding (externals
+#ifdef MyBuildDict
+                                          , buildDictionaryT
+#endif
+                                                            )
 import HERMIT.External (External,external)
 import HERMIT.GHC
 import HERMIT.Kure
 import HERMIT.Plugin (hermitPlugin,phase,interactive)
+
+#ifdef MyBuildDict
+-- For experimental buildDictionaryT tweaks
+import Control.Monad (when)
+import Data.Char (isSpace)
+import HERMIT.Monad (HermitM,liftCoreM,getModGuts,newIdH)
+#endif
 
 -- | e ==> succ e, if e has a type in Enum
 succR :: RewriteH CoreExpr
@@ -33,3 +47,16 @@ externals = [external "succ" (promoteR succR :: RewriteH Core) ["..."]]
 
 plugin :: Plugin
 plugin = hermitPlugin (phase 0 . interactive externals)
+
+#ifdef MyBuildDict
+buildDictionaryT :: Transform c HermitM Type CoreExpr
+buildDictionaryT = contextfreeT $ \ ty -> do
+    dflags <- getDynFlags
+    binder <- newIdH ("$d" ++ filter (not . isSpace) (showPpr dflags ty)) ty
+    guts <- getModGuts
+    (i,bnds) <- liftCoreM $ buildDictionary guts binder
+    when (null bnds) $ fail "couldn't build dictionary"
+    return $ case bnds of
+                [NonRec v e] | i == v -> e -- the common case that we would have gotten a single non-recursive let
+                _ -> mkCoreLets bnds (varToCoreExpr i)
+#endif
